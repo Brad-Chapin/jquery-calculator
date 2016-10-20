27c4b4ac17429d91f46cf34a25544741542af06c
Brad Chapin <paledoor@yahoo.com>
2016-10-20 15:00:35 -0600

    working calculator using eval()

--- js/app.js
+++ js/app.js
@@ -1,10 +1,54 @@
 $( document ).ready(function() {
-  var buttons = $(".buttons");
-  var display = $("#screen");
-    buttons.on("click", function(event){
-      var target = $(event.target);
-      if (target.tagName == "span"){
-        console.log(target);
-      }
-    });
+  var $buttons = $(".buttons").children();
+  for (var i = 0; i < $buttons.length; i++){
+    $($buttons[i]).on("click", clicks);
+  }
 });
+function clicks (event){
+  var $display = $("#screen");
+  var $target = $(event.target);
+
+  switch (true){
+
+  //  if a number is clicked
+  case (!isNaN($target.html())):
+  console.log("number was clicked!!!");
+  $($display).text($display.html() + $target.html());
+  break;
+
+// if the clear button is clicked
+  case ($target.html() == "C"):
+  console.log("clear the input");
+  $display.text("");
+  break;
+
+// if math is ready to be performed
+  case ($target.html() == "="):
+  console.log("clicked equal sign");
+  $display.text(eval($display.html()));
+  break;
+
+  //if + is clicked
+  case ($target.html() == "+"):
+  console.log("operator was clicked");
+  $($display).text($display.html() + "+");
+  break;
+
+  //if - is clicked
+  case ($target.html() == "-"):
+  console.log("operator was clicked");
+  $($display).text($display.html() + "-");
+  break;
+
+  //if x is clicked
+  case ($target.html() == "x"):
+  console.log("operator was clicked");
+  $($display).text($display.html() + "*");
+  break;
+
+  //if ÷ is clicked
+  case ($target.html() == "÷"):
+  console.log("operator was clicked");
+  $($display).text($display.html() + "/");
+  }
+};
